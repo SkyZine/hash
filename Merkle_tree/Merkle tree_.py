042f68da7948b1b871ebeeb7fe2d6f5fa9720fca
sky <91472771+SkyZine@users.noreply.github.com>
2022-07-28 16:46:58 +0800

    Add files via upload

---
+++ Merkle_tree/Merkle tree_.py
@@ -0,0 +1,40 @@
+# coding:utf-8
+import hashlib
+
+def hash_data(data, hash_function = 'sha256'):
+    "hash function"
+    hash_function = getattr(hashlib, hash_function)
+    data = data.encode('utf-8')
+    return hash_function(data).hexdigest()
+
+def concat_and_hash_list(lst, hash_function = 'sha256'):
+    lst1 = []
+    for i in lst:
+        lst1.append(hash_data(i))
+    # print(lst1)
+
+    assert len(lst1)>2, "no tracnsactions to be hashed"
+    n = 0 #merkle树高度
+    while len(lst1) >1:
+        n += 1
+        if len(lst1)%2 == 0:
+            v = []
+            while len(lst1) >1 :
+                a = lst1.pop(0)
+                b = lst1.pop(0)
+                v.append(hash_data(a+b, hash_function))
+            lst1 = v
+        else:
+            v = []
+            l = lst1.pop(-1)
+            while len(lst1) >1 :
+                a = lst1.pop(0)
+                b = lst1.pop(0)
+                v.append(hash_data(a+b, hash_function))
+            v.append(l)
+            lst1 = v
+    return lst1, n+1
+
+
+l = ['a', 'b', 'c',"d"]
+print(concat_and_hash_list(l))
